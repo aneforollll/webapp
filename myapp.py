0d7afd7b50a0f13dc022480e15750999bb0b0dd7
Sithinon Techkarnjanaruk <sithinon2006@gmail.com>
2024-01-23 04:23:52 +0700

    needhelp

--- myapp.py
+++ myapp.py
@@ -1,5 +1,5 @@
 import pandas as pd 
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pickle as pkl
@@ -9,7 +9,7 @@ X_data = data.drop(columns=['HeartDisease'])
 Y_data = data['HeartDisease']
 X_data_train,X_data_test,Y_data_train,Y_data_test = train_test_split(X_data,Y_data, test_size=0.2)
 
-model = RandomForestClassifier()
+model = DecisionTreeClassifier()
 model.fit(X_data_train,Y_data_train)
 
 pkl.dump(model , open('heart_model.pkl','wb'))
