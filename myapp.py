8a9ed6145dd98ebe2b1323bc906ea1f36ded7cdf
Sithinon Techkarnjanaruk <sithinon.tec@student.mahidol.ac.th>
2026-02-24 21:56:09 +0700

    fix bug

--- myapp.py
+++ myapp.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pickle as pkl
 
-data = pd.read_csv('heart_cleaned.csv')
+data = pd.read_csv('C:\Gitthub\webapp\heart_cleaned.csv')
 X_data = data.drop(columns=['HeartDisease'])
 Y_data = data['HeartDisease']
 X_data_train,X_data_test,Y_data_train,Y_data_test = train_test_split(X_data,Y_data, test_size=0.2)
